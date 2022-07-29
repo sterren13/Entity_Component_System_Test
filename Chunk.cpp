172885d2993cbde3fd526a6fd33fb836a088094b
sterren13 <sterren13@gamil.com>
2022-07-29 15:30:15 +0200

    start project

---
+++ Chunk.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sterr on 29/07/2022.
+//
+
+#include "Chunk.h"
---
+++ TypeIdGenerator.h
@@ -0,0 +1,30 @@
+//
+// Created by sterr on 29/07/2022.
+//
+
+#ifndef ENTITY_COMPONENT_SYSTEM_TEST_TYPEIDGENERATOR_H
+#define ENTITY_COMPONENT_SYSTEM_TEST_TYPEIDGENERATOR_H
+
+#include <cstdint>
+
+typedef std::uint32_t IDType;
+typedef IDType EntityID;
+typedef IDType ComponentTypeID;
+const IDType NULL_ENTITY = 0;
+
+template<class T>
+class TypeIdGenerator{
+public:
+    template<class U>
+    static const IDType GetNewID(){
+        static const IDType idCounter = m_count++;
+        return idCounter;
+    }
+private:
+    static IDType m_count;
+};
+
+template<class T> IDType TypeIdGenerator<T>::m_count = 0;
+
+
+#endif //ENTITY_COMPONENT_SYSTEM_TEST_TYPEIDGENERATOR_H
